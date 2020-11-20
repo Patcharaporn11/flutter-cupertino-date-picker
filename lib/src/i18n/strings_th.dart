36fcd36d8318d7acdfaba0a6afc4bcab5e288762
patcharaporn <patcharaporn.surot@gmail.com>
2020-11-20 13:32:56 +0700

    Add thai language

---
+++ lib/src/i18n/strings_th.dart
@@ -0,0 +1,78 @@
+part of 'date_picker_i18n.dart';
+
+/// Spanish (ES)
+class _StringsTH extends _StringsI18n {
+  const _StringsTH();
+
+  @override
+  String getCancelText() {
+    return 'ยกเลิก';
+  }
+
+  @override
+  String getDoneText() {
+    return 'คกลง';
+  }
+
+  @override
+  List<String> getMonths() {
+    return [
+      "มกราคม",
+      "กุมภาพันธ์",
+      "มีนาคม",
+      "เมษายน",
+      "พฤษภาคม",
+      "มิถุนายน",
+      "กรกฎาคม",
+      "สิงหาคม",
+      "กันยายน",
+      "ตุลาคม",
+      "พฤศจิกายน",
+      "ธันวาคม"
+    ];
+  }
+
+  @override
+  List<String> getMonthsShort() {
+    return [
+      "ม.ค.",
+      "ก.พ.",
+      "มี.ค.",
+      "เม.ย.",
+      "พ.ค.",
+      "มิ.ย.",
+      "ก.ค.",
+      "ส.ค.",
+      "ก.ย.",
+      "ต.ค.",
+      "พ.ย.",
+      "ธ.ค.",
+    ];
+  }
+
+  @override
+  List<String> getWeeksFull() {
+    return [
+      "จันทร์",
+      "อังคาร",
+      "พุธ",
+      "พฤหัสบดี",
+      "ศุกร์",
+      "เสาร์",
+      "อาทิตย์",
+    ];
+  }
+
+  @override
+  List<String> getWeeksShort() {
+    return [
+      "จันทร์",
+      "อังคาร",
+      "พุธ",
+      "พฤหัสบดี",
+      "ศุกร์",
+      "เสาร์",
+      "อาทิตย์",
+    ];
+  }
+}
